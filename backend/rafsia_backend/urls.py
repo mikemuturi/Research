9164d9781539b0868d21250de6abdb255e50fe8a
mikemuturi <mikeirungumuturi@gmail.com>
2025-08-12 11:04:53 +0300

    I have addded env

--- backend/rafsia_backend/urls.py
+++ backend/rafsia_backend/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import home 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/surveys/', include('surveys.urls')),
-    path('api/interviews/', include('interviews.urls')),
+    path("", home, name="home"), 
+    path("admin/", admin.site.urls),
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/surveys/", include("surveys.urls")),
+    path("api/interviews/", include("interviews.urls")),
 ]
